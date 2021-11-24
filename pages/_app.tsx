32650600ede6e18f991abb2e481162fa8f3f0821
Alvaro_Aquije <alvarodevcode@outlook.com>
2021-11-24 12:36:40 -0500

    feat(globalApp): :lipstick: applied custom theming

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import { CustomGlobalStyles, customTheme } from '../styles/theme.config';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={customTheme}>
+      <CustomGlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ styles/theme.config.ts
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const CustomGlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-size: 12px;
+    border: none;
+    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; 
+  }
+
+  body {
+    background-color:${({ theme }: { theme: typeof customTheme }) =>
+      theme.darkBg};
+    color: ${({ theme }: { theme: typeof customTheme }) => theme.mainText};
+  } 
+`;
+
+export const customTheme = {
+  // Colors
+  mainText: '#ffffff',
+  accentText: '#94979A',
+  darkBg: '#222528',
+  accentBg: '#2C2F33',
+  primaryClrRed: '#DA584B',
+  secondaryClrGreen: '#70B252',
+  tertiaryClrYellow: '#E5B454',
+
+  // Typography
+  primaryFontSize: '15px',
+};
