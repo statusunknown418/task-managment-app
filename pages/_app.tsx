8db7295b404910097eb6fcf0540862a2cd7ee2a3
Alvaro_Aquije <alvarodevcode@outlook.com>
2021-11-24 15:39:26 -0500

    feat(theme): :lipstick: added theme and icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import { CustomGlobalStyles, customTheme } from '../styles/theme.config';
+import { Layout } from '../components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={customTheme}>
       <CustomGlobalStyles />
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ThemeProvider>
   );
 }
