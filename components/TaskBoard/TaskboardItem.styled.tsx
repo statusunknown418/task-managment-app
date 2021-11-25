96419ad467cdf5188860718eb1973b45b0892025
Alvaro_Aquije <alvarodevcode@outlook.com>
2021-11-25 16:40:08 -0500

    refactor(components): :fire: removed repeated styles and refactored components

--- components/TaskBoard/TaskboardItem.styled.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const TaskboardItemStyled = styled.article`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 16px;
-`;
--- components/TaskNavbar/TaskNavbar.styled.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const TaskNavbarStyled = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 20px;
-  margin-top: 36px;
-`;
