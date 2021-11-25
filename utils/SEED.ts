6dd19a46b06245b3b62ae5e038e66624252a0c4c
Alvaro_Aquije <alvarodevcode@outlook.com>
2021-11-25 10:55:20 -0500

    chore(utils): :building_construction: extracted seed constants

---
+++ utils/SEED.ts
@@ -0,0 +1,16 @@
+export const SEED_NOTIFICATIONS = [
+  {
+    id: 1,
+    title: 'New task assigned to you',
+    description: 'You have a new task',
+    date: '2020-01-01',
+    read: false,
+  },
+  {
+    id: 2,
+    title: 'New task',
+    description: 'You have a new task',
+    date: '2020-01-01',
+    read: false,
+  },
+];
