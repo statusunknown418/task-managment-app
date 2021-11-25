cf5e5471ea71238927f859089eb7338a8a0b6af0
Alvaro_Aquije <alvarodevcode@outlook.com>
2021-11-24 23:38:44 -0500

    feat(graphql): :rocket: apollo provider setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,18 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import { CustomGlobalStyles, customTheme } from '../styles/theme.config';
 import { Layout } from '../components/Layout';
+import { ApolloProvider } from '@apollo/client';
+import { apolloClient } from '../graphql/client';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={customTheme}>
       <CustomGlobalStyles />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <ApolloProvider client={apolloClient}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ApolloProvider>
     </ThemeProvider>
   );
 }
