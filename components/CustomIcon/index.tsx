edeb20a4eb296031fc06a8a66706ea20231f30c0
Alvaro_Aquije <alvarodevcode@outlook.com>
2021-11-27 12:15:42 -0500

    style(components): :lipstick: added styling and functionality to the task card

---
+++ components/CustomIcon/index.tsx
@@ -0,0 +1,13 @@
+import { SVGProps } from 'react';
+
+export const CustomIcon = (props: any) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" {...props} fill="currentColor">
+      <path
+        width={props.width}
+        height={props.height}
+        d="M11 20a9 9 0 110-18 9 9 0 010 18zm0-2a7 7 0 100-14 7 7 0 000 14zm1-7h3v2h-5V6h2v5zM.747 4.282L4.282.747l1.415 1.414L2.16 5.697.747 4.282zM17.717.747l3.536 3.535-1.414 1.415-3.536-3.536L17.718.747h-.001z"
+      />
+    </svg>
+  );
+};
